abb903b7e8637c6514b2e821b08e3d826c6b886c
Lameen Williams <lajrwilliams00@gmail.com>
2024-05-22 18:53:53 -0400

    Fix links in the footer

--- src/app/componets/footer.tsx
+++ src/app/componets/footer.tsx
@@ -15,30 +15,30 @@ function Footer() {
                 <div>
                     <ul className="text-gray-500 dark:text-gray-400 font-medium">
                         <li className="mb-4">
-                            <a href="https://flowbite.com/" className="hover:underline">News</a>
+                            <a href="/news" className="hover:underline">News</a>
                         </li>
                         <li>
-                            <a href="https://tailwindcss.com/" className="hover:underline">Schedule</a>
+                            <a href="/schedule" className="hover:underline">Schedule</a>
                         </li>
                     </ul>
                 </div>
                 <div>
                     <ul className="text-gray-500 dark:text-gray-400 font-medium">
                         <li className="mb-4">
-                            <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Roster</a>
+                            <a href="/roster" className="hover:underline ">Roster</a>
                         </li>
                         <li>
-                            <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Stats</a>
+                            <a href="/stats" className="hover:underline">Stats</a>
                         </li>
                     </ul>
                 </div>
                 <div>
                     <ul className="text-gray-500 dark:text-gray-400 font-medium">
                         <li className="mb-4">
-                            <a href="#" className="hover:underline">Coaches</a>
+                            <a href="/coaches" className="hover:underline">Coaches</a>
                         </li>
                         <li>
-                            <a href="#" className="hover:underline">About Us</a>
+                            <a href="/about" className="hover:underline">About Us</a>
                         </li>
                     </ul>
                 </div>
@@ -46,7 +46,7 @@ function Footer() {
         </div>
     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
     <div className="sm:flex sm:items-center sm:justify-between">
-        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://flowbite.com/" className="hover:underline">NC Venom</a>. All Rights Reserved.
+        <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="" className="hover:underline">NC Venom</a>. All Rights Reserved.
         </span>
         <div className="flex gap-4  mt-4 sm:justify-center sm:mt-0">
             <a href="https://www.facebook.com/profile.php?id=100054646651802">
